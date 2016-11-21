f71c72d7ccb3a219db2b85169e2425237f68d1f0
Aleš Roubíček <ales@roubicek.name>
2016-11-21 15:41:14 +0100

    Get rig of table layout in match

--- src/Suave.IIS/Configuration.fs
+++ src/Suave.IIS/Configuration.fs
@@ -11,11 +11,11 @@ type Setup = {
 /// Parse configuration setup from command line args
 let parseSetup (args:string []) =
     match args |> Array.length with
-    | x when x >= 2     -> { Port = uint16 args.[0]; Path = args.[1] } |> Some
-    | _                 -> None
+    | x when x >= 2 -> { Port = uint16 args.[0]; Path = args.[1] } |> Some
+    | _  -> None
 
 /// Set Suave port based on command line args setup
 let withPort (args:string []) config =
     match args |> parseSetup with
-    | Some(setup)   -> { config with bindings=[HttpBinding.mk HTTP IPAddress.Any setup.Port]}
-    | None          -> config
+    | Some(setup) -> { config with bindings=[HttpBinding.mk HTTP IPAddress.Any setup.Port]}
+    | None -> config
